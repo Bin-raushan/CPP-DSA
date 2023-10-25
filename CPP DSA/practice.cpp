13d4a6495dfcd7008e8f074f78c331f5b180ec82
Raushan Kumar <kumarraushan08122000@gmail.com>
2023-10-25 09:26:49 +0530

    change in code

--- CPP DSA/practice.cpp
+++ CPP DSA/practice.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"this is practice section";
+    cout<<"this is practice section"<<endl;;
+    int a;
+    int b;
+    cin>>a>>b;
+    int sum;
+    sum=a+b;
+    cout<<"sum="<<sum;
     return 0;
 }
